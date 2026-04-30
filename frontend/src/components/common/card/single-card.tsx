67e523eb685143f4f9085b84d038bf5c125a904b
Jyoti Nayak <nayakjyoti789@gmail.com>
2026-04-30 22:15:20 +0545

    cart

--- frontend/src/components/common/card/single-card.tsx
+++ frontend/src/components/common/card/single-card.tsx
@@ -1,4 +1,4 @@
-import { Button, Card } from "flowbite-react";
+import { Card } from "flowbite-react";
 import SingleCardWithImageAndTitleProps from "./single-card.contracts";
 import ProductCardProps from "./product-card.contracts";
 import { FaShoppingCart } from "react-icons/fa";
